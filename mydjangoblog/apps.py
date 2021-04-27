a00f0b07ce328f79a40620fa88e749db4b848b54
DexyTheAwesome <fashanufadeke@gmail.com>
2021-04-27 23:50:04 +0100

    A simple blog app made with Django

---
+++ mydjangoblog/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MydjangoblogConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mydjangoblog'
---
+++ mydjangoblog/forms.py
@@ -0,0 +1,16 @@
+from .models import Comment
+from django import forms
+
+
+class CommentForm(forms.ModelForm):
+    content = forms.CharField(label ='', widget = forms.Textarea(
+        attrs = {
+            'class':'form-control',
+            'placeholder':"Comment here!",
+            'rows':4,
+            'cols':50
+        }
+    ))
+    class Meta:
+        model = Comment
+        fields = ('post','author', 'comment')
