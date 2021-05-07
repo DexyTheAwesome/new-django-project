e59953b1eb4725e714be179e3200198055c8e53a
DexyTheAwesome <fashanufadeke@gmail.com>
2021-05-07 05:25:31 +0100

    update

--- zuridjangoproject/settings.py
+++ zuridjangoproject/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
+import environ
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool, default=True)
+DEBUG = os.environ.get('DEBUG', cast=bool, default=True)
 
 ALLOWED_HOSTS = ['calm-shelf-42884.herokuapp.com', 'localhost', '127.0.0.1']
 
